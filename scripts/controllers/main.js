b311851de2b3326881510f1f964390ed6769d998
Aaron Weiss <aiceweiss@hotmail.com>
2017-07-31 19:18:56 -0400

    Test

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,5 +1,9 @@
 // create the controller and inject Angular's $scope
-angular.module('ngRoute').controller('mainController', ['$scope', '$http', function ($scope, $http) {
-    // create a message to display in our view
+angular
+    .module('ngRoute')
+    .controller('mainController', mainController($scope, $http));
+
+function mainController($scope, $http) {
     $scope.message = 'Aspiring full-stack developer; hobbyist game developer.';
-}]);
+}
+    
