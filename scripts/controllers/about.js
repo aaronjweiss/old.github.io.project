30184c22a7a0774893d0a310305617d7d6d75977
Aaron Weiss <aiceweiss@hotmail.com>
2017-07-31 18:56:22 -0400

    Adjust

--- scripts/controllers/about.js
+++ scripts/controllers/about.js
@@ -1,5 +1,5 @@
 // create the controller and inject Angular's $scope
-angular.module('myApp').controller('aboutController', ['$scope', '$http', function ($scope, $http) {
+angular.module('myApp').controller('aboutController', function () {
     // create a message to display in our view
     $scope.message = 'About me.';
-}]);
+});
