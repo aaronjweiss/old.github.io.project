36fd9b1e064a36f4ffc1040d5f27c1c5183940d0
Aaron Weiss <aiceweiss@hotmail.com>
2017-08-07 20:16:40 -0400

    Blog added

--- scripts/controllers/blog.js
+++ scripts/controllers/blog.js
@@ -4,7 +4,7 @@
 app
 
     .controller('blogController', ['$scope', function ($scope) {
-        $scope.orightml = '<h2>Blog away!</h2>>';
+        $scope.orightml = '';
         $scope.htmlcontent = $scope.orightml;
         $scope.disabled = false;
     }]);
